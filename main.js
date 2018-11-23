53e9f31e259c0d0c410aa9234eb1f5c3cd72826a
MassiBaldi <massibaldi85@gmail.com>
2018-11-23 17:19:34 +0100

    fatto l'array con tutti i giocatori e proprietà

--- main.js
+++ main.js
@@ -7,19 +7,39 @@
 // - Percentuale di successo per tiri da 2 punti
 // - Percentuale di successo per da 3 punti
 // Una volta generato il “database”, il programma deve chiedere all’utente di inserire un Codice Giocatore e il programma restituisce le statistiche.
+
 // creo un array vuoto
 var giocatori = [];
 //ciclo for per popolare l'array dei 100giocatori
 for(var i = 1; i <= 100; i++) {
-  giocatore = {
-    giocatoreUnivoco: 0,
-    numeroPunti: 0,
-    numeroRimbalzi: 0,
-    falli: 0,
-    percentuale2punti: 0,
-    percentuale3punti: 0
-  },
+  var giocatore = {
+    'giocatoreUnivoco': univoco(),
+    'numeroPunti': getRandomNumber(1, 50),
+    'numeroRimbalzi': getRandomNumber(1, 50),
+    'falli': getRandomNumber(1, 10),
+    'percentuale2punti': getRandomNumber(15, 90) + '%',
+    'percentuale3punti': getRandomNumber(15, 90) + '%'
+  };
   // console.log('giocatore ' + i + ' ' +giocatore);
-  giocatori.push('giocatore ' + i + ' ' +giocatore);
+  giocatori.push(giocatore);
 }
+
 console.log(giocatori);
+// document.writeln('Player '+ giocatori[key]);
+function getRandomNumber(min, max) {
+  var newNumeroRandom = Math.floor(Math.random() * (max - min + 1) + min);
+  return newNumeroRandom;
+}
+// creo una funzione che mi restituisca tre lettere maiuscole e tre numeri randomici
+function univoco() {
+  var gUnivoco = '';
+  var lettere_list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numeri_list = '0123456789';
+  for(var i=0; i < 3; i++ ) {
+  gUnivoco += lettere_list.charAt(Math.floor(Math.random() * lettere_list.length));
+  }
+  for(var i=0; i < 3; i++ ) {
+  gUnivoco += numeri_list.charAt(Math.floor(Math.random() * numeri_list.length));
+  }
+  return gUnivoco;
+}
